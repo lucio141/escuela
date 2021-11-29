419caa899c061594fb240ff6976fb0fccef7f5d5
Axel Baconsky <axelbaco@hotmail.com>
2021-11-29 20:43:33 -0300

    Clase Rol hecha

---
+++ src/main/java/com/example/demo/entidades/Rol.java
@@ -0,0 +1,52 @@
+package com.example.demo.entidades;
+
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+
+@Entity
+@SQLDelete(sql="UPDATE Rol r SET r.alta = false WHERE r.id = ?")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    @Column(nullable = false)
+    private String nombre;
+    private Boolean alta;
+
+    public Rol(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.alta = true;
+    }
+
+    public Rol() {
+        this.alta = true;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Boolean alta) {
+        this.alta = alta;
+    }
+}
