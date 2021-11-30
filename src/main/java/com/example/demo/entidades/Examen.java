b8b28be9a75b1ab09728bfb89edfbf0c535e8cfe
Lucio Grammatico <luciog_141@hotmail.com>
2021-11-29 21:32:18 -0300

    Lucio Commit

---
+++ src/main/java/com/example/demo/entidades/Examen.java
@@ -0,0 +1,112 @@
+package com.example.demo.entidades;
+
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@SQLDelete(sql = "UPDATE Examen e SET e.alta = false WHERE e.id = ?")
+public class Examen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String tipo;
+    private Tematica tematica;
+    private List<Resultado> resultados;
+    private List<Pregunta> preguntas;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date fechaCreacion;
+    @LastModifiedDate
+    private Date fechaModificacion;
+
+    private Boolean alta;
+
+    public Examen(Integer id, String tipo, Tematica tematica, List<Resultado> resultados, List<Pregunta> preguntas, Date fechaCreacion, Date fechaModificacion) {
+        this.id = id;
+        this.tipo = tipo;
+        this.tematica = tematica;
+        this.resultados = resultados;
+        this.preguntas = preguntas;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaModificacion = fechaModificacion;
+        this.alta = true;
+    }
+
+    public Examen() {
+        this.alta = true;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Tematica getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(Tematica tematica) {
+        this.tematica = tematica;
+    }
+
+    public List<Resultado> getResultados() {
+        return resultados;
+    }
+
+    public void setResultados(List<Resultado> resultados) {
+        this.resultados = resultados;
+    }
+
+    public List<Pregunta> getPreguntas() {
+        return preguntas;
+    }
+
+    public void setPreguntas(List<Pregunta> preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+
+    public Boolean getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Boolean alta) {
+        this.alta = alta;
+    }
+    
+}
