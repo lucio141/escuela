3b1188f16afba55b271535a1e4772d21e459410b
Leandro Fontana <you@example.com>
2021-12-01 19:53:55 -0300

    Flujo Usuario

--- src/main/java/com/example/demo/servicios/UsuarioServicio.java
+++ src/main/java/com/example/demo/servicios/UsuarioServicio.java
@@ -1,4 +1,61 @@
 package com.example.demo.servicios;
 
+import com.example.demo.entidades.Rol;
+import com.example.demo.entidades.Usuario;
+import com.example.demo.excepciones.ObjetoNulloExcepcion;
+import com.example.demo.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class UsuarioServicio {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+    @Transactional
+    public void crearUsuario(String nombreUsuario, String contrasenia, String mail, Rol rol){
+        Usuario usuario = new Usuario();
+        usuario.setNombreUsuario(nombreUsuario);
+        usuario.setContrasenia(contrasenia);
+        usuario.setMail(mail);
+        usuario.setRol(rol);
+        usuarioRepositorio.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> mostrarUsuarios(Boolean alta){
+        return usuarioRepositorio.mostrarUsuarios(alta);
+    }
+
+    @Transactional
+    public Usuario buscarPorId (Integer id){
+        return usuarioRepositorio.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void editarUsuario(Integer id, String nombreUsuario, String contrasenia, String mail, Rol rol) throws ObjetoNulloExcepcion{
+        Usuario usuario = usuarioRepositorio.findById(id).orElse(null);
+        if (usuario == null){
+            throw new ObjetoNulloExcepcion("");
+        }
+        usuario.setNombreUsuario(nombreUsuario);
+        usuario.setContrasenia(contrasenia);
+        usuario.setMail(mail);
+        usuario.setRol(rol);
+        usuarioRepositorio.save(usuario);
+    }
+
+    @Transactional
+    public void eliminar(Integer id) {
+        usuarioRepositorio.deleteById(id);
+    }
+
+    @Transactional
+    public void darAlta(Integer id) {
+        usuarioRepositorio.darAlta(id);
+    }
 }
