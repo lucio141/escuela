117a39304d65fc11d1f497a3ce4032a83d50f99f
ChungYao <ch.chungyao@gmail.com>
2021-12-13 21:58:48 -0300

    Error Controlador

--- src/main/java/com/example/demo/utilidades/Utilidad.java
+++ src/main/java/com/example/demo/utilidades/Utilidad.java
@@ -1,6 +1,20 @@
 package com.example.demo.utilidades;
 
+import com.example.demo.excepciones.ValidacionCampExcepcion;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utilidad {
 
     //Aca van los metodos de utilidad como regex (validaciones) por ej
+
+    public void validarUsuario(String nombreUsuario) throws ValidacionCampExcepcion {
+
+        Pattern pat = Pattern.compile("^([-_a-z-ù0-9]+)$");
+        Matcher mat = pat.matcher(nombreUsuario);
+        if (!mat.matches()) {
+            throw new ValidacionCampExcepcion("El formato de usuario es inconrrecto");
+        }
+    }
 }
